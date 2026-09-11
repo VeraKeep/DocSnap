0bd6f999670feac04273a587bffbff53b82ec113
Michael Yourgal <124070491+myourgal25@users.noreply.github.com>
2026-09-10 21:19:23 -0400

    Restore DocSnap after Opinly deployment outage
    * Restore pre-Opinly production build
    
    * Restore pre-Opinly production build
    
    * Restore pre-Opinly production build
    
    * Restore pre-Opinly production build
    
    * Restore pre-Opinly production build
    
    * Restore pre-Opinly production build
    
    * Restore pre-Opinly production build
    
    * Restore pre-Opinly production build
    
    * Restore pre-Opinly production build
    
    * Restore pre-Opinly production build
    
    * Restore pre-Opinly production build
    
    * Restore pre-Opinly production build
    
    * Restore pre-Opinly production build

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,7 +9,6 @@ import { useEffect, type ReactNode } from "react";
 
 import { RouteErrorBoundary } from "~/components/RouteErrorBoundary";
 import { PwaRuntime } from "~/components/PwaRuntime";
-import { OpinlyIdentity } from "~/components/OpinlyIdentity";
 import { installGlobalErrorHandlers } from "~/lib/errorLogger";
 import appCss from "~/styles/app.css?url";
 import { assetUrl } from "~/siteConfig";
@@ -113,11 +112,6 @@ function RootDocument({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
-        <script
-          async
-          src="https://static.opinly.ai/p.js"
-          data-key="pk-XP7GJCphMxbPo0mGpUO_5V3_6F8R1kuxeRMV7yI"
-        />
         {plausibleDomain ? (
           <script
             defer
@@ -177,7 +171,6 @@ function RootDocument({ children }: { children: ReactNode }) {
           <span>Powered by VeraKeep™</span>
         </footer>
         <PwaRuntime />
-        <OpinlyIdentity />
         <Scripts />
       </body>
     </html>
