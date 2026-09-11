5855860edd982775d017d504c67d841ecac97e84
Michael Yourgal <124070491+myourgal25@users.noreply.github.com>
2026-09-10 21:18:17 -0400

    Restore pre-Opinly production build

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -675,9 +675,6 @@ export function LandingPage({
         <Link to="/pricing" className="transition hover:text-indigo-400">
           Pricing
         </Link>
-        <Link to="/blog" className="transition hover:text-indigo-400">
-          Blog
-        </Link>
       </footer>
     </div>
   );
